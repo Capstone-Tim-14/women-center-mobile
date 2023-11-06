de618313efd74b94daf17d978e49a0415259f64e
juharmansah <juharmansah0603@gmail.com>
2023-11-06 21:33:13 +0700

    create Routes

---
+++ women_center_mobile/lib/View/login/login.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Logi extends StatelessWidget {
+  const Logi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
