480fdc2c18f4d932f461cb1c672aab6c05113bed
juharmansah <juharmansah0603@gmail.com>
2023-11-07 18:12:58 +0700

    Feature(file konseling): menambahkan filr file pada folder konseling pada main

---
+++ women_center_mobile/lib/View/konseling/konseling_page_pilihan_paket.dart
@@ -0,0 +1,12 @@
+// PUNYA JUHARMANSAH
+
+import 'package:flutter/material.dart';
+
+class KonselingPagePilihanPaket extends StatelessWidget {
+  const KonselingPagePilihanPaket({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
