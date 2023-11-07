68eafe58cca0500d581a0b1f8b418b60815d6e89
juharmansah <juharmansah0603@gmail.com>
2023-11-07 18:00:59 +0700

    p

--- women_center_mobile/lib/View/login/login.dart
+++ women_center_mobile/lib/View/login/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Logi extends StatelessWidget {
-  const Logi({super.key});
+class Login extends StatelessWidget {
+  const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
