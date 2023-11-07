6b5d8be3419dcad223cdac4634d73a23d52d6b9d
juharmansah <juharmansah0603@gmail.com>
2023-11-07 10:18:20 +0700

    update main

--- women_center_mobile/lib/main.dart
+++ women_center_mobile/lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:women_center_mobile/View/onboarding/onboarding.dart';
 import 'package:women_center_mobile/View/splash_screen/splash_screen.dart';
-
 import 'View/login/login.dart';
 import 'View/register/register.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title:'women center',
-      theme: ThemeData(),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink)
+      ),
 
     initialRoute: '/',
     routes: {
