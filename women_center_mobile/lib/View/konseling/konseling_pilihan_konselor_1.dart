3da618e4e64777f5d93e68f5e704ac22c1b5bfc6
Xrtion <kicaubae@gmail.com>
2023-11-08 23:45:35 +0700

    updated

--- women_center_mobile/lib/View/konseling/konseling_pilihan_konselor_1.dart
+++ women_center_mobile/lib/View/konseling/konseling_pilihan_konselor_1.dart
@@ -1,8 +1,16 @@
-//PUNYA David Liem
-
+//David Liem
 import 'package:flutter/material.dart';
 
-class konseling_pilihan_konselor_1 extends StatelessWidget {
+class konseling_pilihan_konselor_1 extends StatefulWidget {
+  @override
+  _konseling_pilihan_konselor_1State createState() =>
+      _konseling_pilihan_konselor_1State();
+}
+
+class _konseling_pilihan_konselor_1State
+    extends State<konseling_pilihan_konselor_1> {
+  Color _warnaTrauma = Colors.white;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,41 +32,74 @@ class konseling_pilihan_konselor_1 extends StatelessWidget {
       body: Container(
         color: Colors.grey[200], // Warna latar belakang abu-abu
         padding: EdgeInsets.all(16.0),
-        child: Row(
+        child: Column(
           children: [
-            Expanded(
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: "Cari nama psikolog",
-                  prefixIcon: Icon(
-                    Icons.search, // Icon di dalam TextField
-                    color: Color(0xFF979797), // Warna ikon
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: "Cari nama psikolog",
+                      prefixIcon: Icon(
+                        Icons.search, // Icon di dalam TextField
+                        color: Color(0xFF979797), // Warna ikon
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      contentPadding: EdgeInsets.all(10.0),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                        borderSide: BorderSide.none, // Hapus outline
+                      ),
+                    ),
                   ),
-                  filled: true,
-                  fillColor: Colors.white,
-                  contentPadding: EdgeInsets.all(10.0),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                    borderSide: BorderSide.none, // Hapus outline
+                ),
+                SizedBox(width: 15.0), // Jarak antara ikon dan TextField
+                InkWell(
+                  onTap: () {
+                    // Tambahkan logika ketika tombol ditekan di sini
+                  },
+                  child: Container(
+                    padding: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    child: Icon(
+                      Icons.filter_list, // Icon di sebelah kanan search bar
+                      color: Colors.black, // Warna ikon
+                    ),
                   ),
+                ),
+              ],
+            ),
+            SizedBox(height: 25.0), // Jarak antara search bar dan teks "Pilihan topik"
+            Align(
+              alignment: Alignment.centerLeft, // Menempatkan teks ke kiri
+              child: Text(
+                "Pilihan topik",
+                style: TextStyle(
+                  fontSize: 16.0,
                 ),
               ),
             ),
-            SizedBox(width: 15.0), // Jarak antara ikon dan TextField
-            InkWell(
-              onTap: () {
-                // Tambahkan logika ketika tombol ditekan di sini
-              },
-              child: Container(
-                padding: EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
-                child: Icon(
-                  Icons.filter_list, // Icon di sebelah kanan search bar
-                  color: Colors.black, // Warna ikon
-                ),
+            SizedBox(height: 25.0),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _warnaTrauma = Colors.pink;
+                      });
+                    },
+                    child: Text('Trauma', style: TextStyle(color: Colors.black, fontSize: 14)),
+                    style: ElevatedButton.styleFrom(
+                      primary: _warnaTrauma,
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
