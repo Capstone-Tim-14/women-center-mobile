42bceadaf093878d4bff8505791e1b06b3b32384
Xrtion <kicaubae@gmail.com>
2023-11-08 22:53:24 +0700

    Search Bar

--- women_center_mobile/lib/View/konseling/konseling_pilihan_konselor_1.dart
+++ women_center_mobile/lib/View/konseling/konseling_pilihan_konselor_1.dart
@@ -3,10 +3,67 @@
 import 'package:flutter/material.dart';
 
 class konseling_pilihan_konselor_1 extends StatelessWidget {
-  const konseling_pilihan_konselor_1({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink[100],
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {},
+        ),
+        title: Center(
+          child: Text(
+            "Konseling",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        color: Colors.grey[200], // Warna latar belakang abu-abu
+        padding: EdgeInsets.all(16.0),
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Cari nama psikolog",
+                  prefixIcon: Icon(
+                    Icons.search, // Icon di dalam TextField
+                    color: Color(0xFF979797), // Warna ikon
+                  ),
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: EdgeInsets.all(10.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                    borderSide: BorderSide.none, // Hapus outline
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 15.0), // Jarak antara ikon dan TextField
+            InkWell(
+              onTap: () {
+                // Tambahkan logika ketika tombol ditekan di sini
+              },
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                child: Icon(
+                  Icons.filter_list, // Icon di sebelah kanan search bar
+                  color: Colors.black, // Warna ikon
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- women_center_mobile/lib/main.dart
+++ women_center_mobile/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:women_center_mobile/View/konseling/konseling_pilihan_konselor_1.dart';
 import 'package:women_center_mobile/View/onboarding/onboarding.dart';
 import 'package:women_center_mobile/View/splash_screen/splash_screen.dart';
 import 'View/login/login.dart';
 import 'View/register/register.dart';
+void main(){
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -16,14 +20,14 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink)
       ),
-
-    initialRoute: '/',
-    routes: {
-      '/': (context) => const Splashscreen(),
-      '/logim': (context) => const Login(),
-      '/register': (context) => const Register(),
-      '/onboarding': (context) => const Onboarding(),
-    }
+      initialRoute: '/',
+      routes: {
+        '/': (context) => konseling_pilihan_konselor_1(),
+        // '/': (context) => const Splashscreen(),
+        // '/logim': (context) => const Login(),
+        // '/register': (context) => const Register(),
+        // '/onboarding': (context) => const Onboarding(),
+      }
     );
   }
 }
