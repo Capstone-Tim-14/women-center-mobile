23e3d35f45efb47c6a9aca6284ae5368bb623a0a
Xrtion <kicaubae@gmail.com>
2023-11-09 17:53:39 +0700

    updated

--- women_center_mobile/lib/View/konseling/konseling_pilihan_konselor_1.dart
+++ women_center_mobile/lib/View/konseling/konseling_pilihan_konselor_1.dart
@@ -1,4 +1,3 @@
-//David Liem
 import 'package:flutter/material.dart';
 
 class konseling_pilihan_konselor_1 extends StatefulWidget {
@@ -9,7 +8,11 @@ class konseling_pilihan_konselor_1 extends StatefulWidget {
 
 class _konseling_pilihan_konselor_1State
     extends State<konseling_pilihan_konselor_1> {
-  Color _warnaTrauma = Colors.white;
+  Color _warna1 = Colors.white;
+  Color _warna2 = Colors.white;
+  Color _warna3 = Colors.white;
+  Color _warna4 = Colors.white;
+  Color _warna5 = Colors.white;
 
   @override
   Widget build(BuildContext context) {
@@ -29,80 +32,108 @@ class _konseling_pilihan_konselor_1State
           ),
         ),
       ),
-      body: Container(
-        color: Colors.grey[200], // Warna latar belakang abu-abu
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      labelText: "Cari nama psikolog",
-                      prefixIcon: Icon(
-                        Icons.search, // Icon di dalam TextField
-                        color: Color(0xFF979797), // Warna ikon
-                      ),
-                      filled: true,
-                      fillColor: Colors.white,
-                      contentPadding: EdgeInsets.all(10.0),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30.0),
-                        borderSide: BorderSide.none, // Hapus outline
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 15.0), // Jarak antara ikon dan TextField
-                InkWell(
-                  onTap: () {
-                    // Tambahkan logika ketika tombol ditekan di sini
-                  },
-                  child: Container(
-                    padding: EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(30.0),
-                    ),
-                    child: Icon(
-                      Icons.filter_list, // Icon di sebelah kanan search bar
-                      color: Colors.black, // Warna ikon
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 25.0), // Jarak antara search bar dan teks "Pilihan topik"
-            Align(
-              alignment: Alignment.centerLeft, // Menempatkan teks ke kiri
-              child: Text(
-                "Pilihan topik",
-                style: TextStyle(
-                  fontSize: 16.0,
-                ),
-              ),
-            ),
-            SizedBox(height: 25.0),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Container(
+          color: Colors.grey[200],
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
                 children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        _warnaTrauma = Colors.pink;
-                      });
-                    },
-                    child: Text('Trauma', style: TextStyle(color: Colors.black, fontSize: 14)),
-                    style: ElevatedButton.styleFrom(
-                      primary: _warnaTrauma,
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: "Cari nama psikolog",
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Color(0xFF979797),
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                        contentPadding: EdgeInsets.all(10.0),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 15.0),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      padding: EdgeInsets.all(10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      child: Icon(
+                        Icons.filter_list,
+                        color: Colors.black,
+                      ),
                     ),
                   ),
                 ],
               ),
-            ),
-          ],
+              SizedBox(height: 25.0),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Pilihan topik",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+              ),
+              SizedBox(height: 15.0),
+              SingleChildScrollView(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _warna1 = (_warna1 == Colors.white) ? const Color.fromRGBO(244, 81, 141, 1) : Colors.white;
+                        });
+                      }, 
+                      child: Text('Trauma', style: TextStyle(color: Colors.black, fontSize: 14)),
+                      style: ElevatedButton.styleFrom(primary: _warna1),
+                    ),
+                    SizedBox(width: 10.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _warna2 = (_warna2 == Colors.white) ? const Color.fromRGBO(244, 81, 141, 1) : Colors.white;
+                        });
+                      }, 
+                      child: Text('Depresi', style: TextStyle(color: Colors.black, fontSize: 14)),
+                      style: ElevatedButton.styleFrom(primary: _warna2),
+                    ),
+                    SizedBox(width: 10.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _warna3 = (_warna3 == Colors.white) ? const Color.fromRGBO(244, 81, 141, 1) : Colors.white;
+                        });
+                      }, 
+                      child: Text('Seksualitas', style: TextStyle(color: Colors.black, fontSize: 14)),
+                      style: ElevatedButton.styleFrom(primary: _warna3),
+                    ),
+                    SizedBox(width: 10.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _warna4 = (_warna4 == Colors.white) ? const Color.fromRGBO(244, 81, 141, 1) : Colors.white;
+                        });
+                      }, 
+                      child: Text('Seksualitas', style: TextStyle(color: Colors.black, fontSize: 14)),
+                      style: ElevatedButton.styleFrom(primary: _warna4),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
