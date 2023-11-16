b4a63c238dc3b3c0a255d49fa2351a6cf1ffcc17
xramadhann <xxdhanz@gmail.com>
2023-11-17 03:01:51 +0700

    Update Home notifikasi

--- women_center_mobile/lib/View/homepage/notifikasi/homepage_notifikasi.dart
+++ women_center_mobile/lib/View/homepage/notifikasi/homepage_notifikasi.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:women_center_mobile/View/homepage/notifikasi/homepage_notifikasi_widget.dart';
 
 class HomepageNotifikasi extends StatefulWidget {
-  const HomepageNotifikasi({super.key});
+  const HomepageNotifikasi({Key? key});
 
   @override
   State<HomepageNotifikasi> createState() => _HomepageNotifikasiState();
@@ -14,11 +14,57 @@ class _HomepageNotifikasiState extends State<HomepageNotifikasi> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(),
-        body: Stack(
-          children: <Widget>[
-            //Diisi sesuai tugas, jangan lupa biar ga conflict kabarin kalau misal mau nambahin sesuatu
-            NotifikasiList()
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          title: const Text(
+            'Notifikasi',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: ListView(
+          children: [
+            ListTile(
+              leading: const Icon(Icons.article),
+              title: const Text('Artikel'),
+              subtitle: const Text('Ini adalah artikel'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NotifikasiList()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.work),
+              title: const Text('Karir'),
+              subtitle: const Text('Info karir dan pekerjaan'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NotifikasiList()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.chat),
+              title: const Text('Konseling'),
+              subtitle: const Text('Bantuan konseling'),
+              onTap: () {
+                // Navigasi ke halaman atau fungsi yang berhubungan dengan 'Konseling'
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => NotifikasiList()),
+                );
+              },
+            ),
           ],
         ),
       ),
