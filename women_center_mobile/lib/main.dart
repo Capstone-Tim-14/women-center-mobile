6b0ac7de5076a7f9661c68443ee0f003f200e013
Yunita Anggeraini <yunitaanggeraini204@gmail.com>
2023-11-18 11:14:42 +0800

    onboarding

--- women_center_mobile/lib/main.dart
+++ women_center_mobile/lib/main.dart
@@ -1,12 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:women_center_mobile/View/splash_screen/splash_screen.dart';
 import 'View/homepage/homepage_section3.dart';
-import 'View/login/login.dart';
+// import 'View/login/login.dart';
 import 'View/onboarding/onboarding.dart';
 import 'View/register/register.dart';
-
-void main(){
-  runApp(const MyApp());
-}
 
 void main() {
   runApp(const MyApp());
