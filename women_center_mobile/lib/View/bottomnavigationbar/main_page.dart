df232fbd6e3f9fc52f8ad532b4038fcca17a7a6e
juharmansah <juharmansah0603@gmail.com>
2023-11-19 11:21:35 +0700

    tinggal atur tampilan, font

--- women_center_mobile/lib/View/bottomnavigationbar/main_page.dart
+++ women_center_mobile/lib/View/bottomnavigationbar/main_page.dart
@@ -37,27 +37,27 @@ class _MainPageState extends State<MainPage> {
         selectedIndex: selectedIndex,
         onItemTapped: (index) => setState(() => selectedIndex = index),
       ),
-      backgroundColor: const Color(0xFFF9F5F6),
-      appBar: AppBar(
-        title: const Text(
-          "Konseling",
-          style: TextStyle(
-            color: Color(0xFfF1F1F1F),
-            fontFamily: 'Raleway',
-            fontWeight: FontWeight.bold,
-            fontSize: 20,
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: const Color(0xFFFDCEDF),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            //tombol kembali mengarah ke home
-            Navigator.of(context).pop();
-          },
-        ),
-      ),
+      // backgroundColor: const Color(0xFFF9F5F6),
+      // appBar: AppBar(
+      //   title: const Text(
+      //     "Konseling",
+      //     style: TextStyle(
+      //       color: Color(0xFfF1F1F1F),
+      //       fontFamily: 'Raleway',
+      //       fontWeight: FontWeight.bold,
+      //       fontSize: 20,
+      //     ),
+      //   ),
+      //   centerTitle: true,
+      //   backgroundColor: const Color(0xFFFDCEDF),
+      //   leading: IconButton(
+      //     icon: const Icon(Icons.arrow_back),
+      //     onPressed: () {
+      //       //tombol kembali mengarah ke home
+      //       Navigator.of(context).pop();
+      //     },
+      //   ),
+      // ),
     );
   }
 }
