af161b095bed79e4ef7280e34ca809d0eeb912f6
juharmansah <juharmansah0603@gmail.com>
2023-11-21 11:13:27 +0700

    create new branch

---
+++ women_center_mobile/lib/View/artikel_2/artikel_2.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Artikel2 extends StatelessWidget {
+  const Artikel2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
