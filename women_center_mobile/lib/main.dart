3fae29f0d1b18f6e59539d46e057adc151d7da4e
Yunita Anggeraini <yunitaanggeraini204@gmail.com>
2023-11-23 04:17:31 +0800

    halaman artikelku

--- women_center_mobile/lib/main.dart
+++ women_center_mobile/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:women_center_mobile/View/artikel/artikel_ku.dart';
 import 'package:women_center_mobile/View/homepage/notifikasi/homepage_notifikasi_3.dart';
 import 'package:women_center_mobile/View/onboarding/onboarding.dart';
 import 'package:women_center_mobile/View/splash_screen/splash_screen.dart';
-import 'View/login/login.dart';
+// import 'View/login/login.dart';
 import 'View/register/register.dart';
 
 
@@ -22,13 +23,14 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink)
       ),
 
-    initialRoute: '/',
+    initialRoute: '/artikelku',
     routes: {
       '/': (context) => const Splashscreen(),
-      '/logim': (context) => const Login(),
+      // '/logim': (context) => const Login(),
       '/register': (context) => const Register(),
       '/onboarding': (context) => Onboarding(),
-      '/homepagenotif3':(context) => HomepageNotifikasi3()
+      '/homepagenotif3':(context) => HomepageNotifikasi3(),
+      '/artikelku':(context) => const ArtikelKu()
     }
     );
   }
