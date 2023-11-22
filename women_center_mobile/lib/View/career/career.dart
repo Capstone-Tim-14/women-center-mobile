d023e372a679382a497c175b182628bad6e4b88c
Xrtion <kicaubae@gmail.com>
2023-11-22 21:53:43 +0700

    updated

--- women_center_mobile/lib/View/career/career.dart
+++ women_center_mobile/lib/View/career/career.dart
@@ -8,20 +8,20 @@ class Career extends StatefulWidget {
 class _CareerState extends State<Career> {
   Color _warna1 = Color.fromARGB(255, 240, 192, 209);
   Color _textColor1 = Color.fromARGB(255, 245, 67, 132);
-  Color _warna2 = Colors.white;
-  Color _textColor2 = Colors.black;
-  Color _warna3 = Colors.white;
-  Color _textColor3 = Colors.black;
-  Color _warna4 = Colors.white;
-  Color _textColor4 = Colors.black;
-  Color _warna5 = Colors.white;
-  Color _textColor5 = Colors.black;
-  Color _warna6 = Colors.white;
-  Color _textColor6 = Colors.black;
-  Color _warna7 = Colors.white;
-  Color _textColor7 = Colors.black;
-  Color _warna8 = Colors.white;
-  Color _textColor8 = Colors.black;
+  Color _warna2 = Color.fromARGB(255, 240, 192, 209);
+  Color _textColor2 = Color.fromARGB(255, 245, 67, 132);
+  Color _warna3 = Color.fromARGB(255, 240, 192, 209);
+  Color _textColor3 = Color.fromARGB(255, 245, 67, 132);
+  Color _warna4 = Color.fromARGB(255, 240, 192, 209);
+  Color _textColor4 = Color.fromARGB(255, 245, 67, 132);
+  Color _warna5 = Color.fromARGB(255, 240, 192, 209);
+  Color _textColor5 = Color.fromARGB(255, 245, 67, 132);
+  Color _warna6 = Color.fromARGB(255, 240, 192, 209);
+  Color _textColor6 = Color.fromARGB(255, 245, 67, 132);
+  Color _warna7 = Color.fromARGB(255, 240, 192, 209);
+  Color _textColor7 = Color.fromARGB(255, 245, 67, 132);
+  Color _warna8 = Color.fromARGB(255, 240, 192, 209);
+  Color _textColor8 = Color.fromARGB(255, 245, 67, 132);
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +34,7 @@ class _CareerState extends State<Career> {
         ),
         title: Center(
           child: Text(
-            "Konseling",
+            "Karir",
             style: TextStyle(
               fontWeight: FontWeight.bold,
             ),
@@ -47,111 +47,115 @@ class _CareerState extends State<Career> {
           padding: EdgeInsets.all(16.0),
           child: Column(
             children: [
+              Column(
+                children: [
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Filter",
+                      style: TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 15.0),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _warna1 = (_warna1 == Color.fromARGB(255, 240, 192, 209)) ? const Color.fromRGBO(244, 81, 141, 1) : Color.fromARGB(255, 240, 192, 209);
+                              _textColor1 = (_textColor1 == Color.fromARGB(255, 245, 67, 132)) ? Colors.white : Color.fromARGB(255, 245, 67, 132); 
+                            });
+                          }, 
+                          child: Text('On Side', style: TextStyle(color: _textColor1, fontSize: 14)),
+                          style: ElevatedButton.styleFrom(primary: _warna1),
+                        ),
+                        SizedBox(width: 10.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _warna2 = (_warna2 == Color.fromARGB(255, 240, 192, 209)) ? const Color.fromRGBO(244, 81, 141, 1) : Color.fromARGB(255, 240, 192, 209);
+                              _textColor2 = (_textColor2 == Color.fromARGB(255, 245, 67, 132)) ? Colors.white : Color.fromARGB(255, 245, 67, 132);
+                            });
+                          }, 
+                          child: Text('Full time', style: TextStyle(color: _textColor2, fontSize: 14)),
+                          style: ElevatedButton.styleFrom(primary: _warna2),
+                        ),
+                        SizedBox(width: 10.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _warna3 = (_warna3 == Color.fromARGB(255, 240, 192, 209)) ? const Color.fromRGBO(244, 81, 141, 1) : Color.fromARGB(255, 240, 192, 209);
+                              _textColor3 = (_textColor3 == Color.fromARGB(255, 245, 67, 132)) ? Colors.white : Color.fromARGB(255, 245, 67, 132);
+                            });
+                          }, 
+                          child: Text('Off side', style: TextStyle(color: _textColor3, fontSize: 14)),
+                          style: ElevatedButton.styleFrom(primary: _warna3),
+                        ),
+                        SizedBox(width: 10.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _warna4 = (_warna4 == Color.fromARGB(255, 240, 192, 209)) ? const Color.fromRGBO(244, 81, 141, 1) : Color.fromARGB(255, 240, 192, 209);
+                              _textColor4 = (_textColor4 == Color.fromARGB(255, 245, 67, 132)) ? Colors.white : Color.fromARGB(255, 245, 67, 132);
+                            });
+                          }, 
+                          child: Text('Half time', style: TextStyle(color: _textColor4, fontSize: 14)),
+                          style: ElevatedButton.styleFrom(primary: _warna4),
+                        ),
+                        SizedBox(width: 10.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _warna6 = (_warna6 == Color.fromARGB(255, 240, 192, 209)) ? const Color.fromRGBO(244, 81, 141, 1) : Color.fromARGB(255, 240, 192, 209);
+                              _textColor6 = (_textColor6 == Color.fromARGB(255, 245, 67, 132)) ? Colors.white : Color.fromARGB(255, 245, 67, 132);
+                            });
+                          }, 
+                          child: Text('Medium level', style: TextStyle(color: _textColor6, fontSize: 14)),
+                          style: ElevatedButton.styleFrom(primary: _warna6),
+                        ),
+                        SizedBox(width: 10.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _warna7 = (_warna7 == Color.fromARGB(255, 240, 192, 209)) ? const Color.fromRGBO(244, 81, 141, 1) : Color.fromARGB(255, 240, 192, 209);
+                              _textColor7 = (_textColor7 == Color.fromARGB(255, 245, 67, 132)) ? Colors.white : Color.fromARGB(255, 245, 67, 132);
+                            });
+                          }, 
+                          child: Text('Advance level', style: TextStyle(color: _textColor7, fontSize: 14)),
+                          style: ElevatedButton.styleFrom(primary: _warna7),
+                        ),
+                        SizedBox(width: 10.0),
+                        ElevatedButton(
+                          onPressed: () {
+                            setState(() {
+                              _warna8 = (_warna8 == Color.fromARGB(255, 240, 192, 209)) ? const Color.fromRGBO(244, 81, 141, 1) : Color.fromARGB(255, 240, 192, 209);
+                              _textColor8 = (_textColor8 == Color.fromARGB(255, 245, 67, 132)) ? Colors.white : Color.fromARGB(255, 245, 67, 132);
+                            });
+                          }, 
+                          child: Text('Entry level', style: TextStyle(color: _textColor8, fontSize: 14)),
+                          style: ElevatedButton.styleFrom(primary: _warna8),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 15),
+              Divider(),
               Align(
                 alignment: Alignment.centerLeft,
                 child: Text(
-                  "Filter",
+                  "For you",
                   style: TextStyle(
                     fontSize: 16.0,
                   ),
                 ),
               ),
-              SizedBox(height: 15.0),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _warna1 = (_warna1 == Colors.white) ? const Color.fromRGBO(244, 81, 141, 1) : Colors.white;
-                          _textColor1 = (_textColor1 == Colors.black) ? Colors.white : Colors.black; 
-                        });
-                      }, 
-                      child: Text('On Side', style: TextStyle(color: _textColor1, fontSize: 14)),
-                      style: ElevatedButton.styleFrom(primary: _warna1),
-                    ),
-                    SizedBox(width: 10.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _warna2 = (_warna2 == Colors.white) ? const Color.fromRGBO(244, 81, 141, 1) : Colors.white;
-                          _textColor2 = (_textColor2 == Colors.black) ? Colors.white : Colors.black;
-                        });
-                      }, 
-                      child: Text('Full time', style: TextStyle(color: _textColor2, fontSize: 14)),
-                      style: ElevatedButton.styleFrom(primary: _warna2),
-                    ),
-                    SizedBox(width: 10.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _warna3 = (_warna3 == Colors.white) ? const Color.fromRGBO(244, 81, 141, 1) : Colors.white;
-                          _textColor3 = (_textColor3 == Colors.black) ? Colors.white : Colors.black;
-                        });
-                      }, 
-                      child: Text('Off side', style: TextStyle(color: _textColor3, fontSize: 14)),
-                      style: ElevatedButton.styleFrom(primary: _warna3),
-                    ),
-                    SizedBox(width: 10.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _warna4 = (_warna4 == Colors.white) ? const Color.fromRGBO(244, 81, 141, 1) : Colors.white;
-                          _textColor4 = (_textColor4 == Colors.black) ? Colors.white : Colors.black;
-                        });
-                      }, 
-                      child: Text('Half time', style: TextStyle(color: _textColor4, fontSize: 14)),
-                      style: ElevatedButton.styleFrom(primary: _warna4),
-                    ),
-                    SizedBox(width: 10.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _warna5 = (_warna5 == Colors.white) ? const Color.fromRGBO(244, 81, 141, 1) : Colors.white;
-                          _textColor5 = (_textColor5 == Colors.black) ? Colors.white : Colors.black;
-                        });
-                      }, 
-                      child: Text('M', style: TextStyle(color: _textColor5, fontSize: 14)),
-                      style: ElevatedButton.styleFrom(primary: _warna5),
-                    ),
-                    SizedBox(width: 10.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _warna5 = (_warna6 == Colors.white) ? const Color.fromRGBO(244, 81, 141, 1) : Colors.white;
-                          _textColor5 = (_textColor6 == Colors.black) ? Colors.white : Colors.black;
-                        });
-                      }, 
-                      child: Text('Medium level', style: TextStyle(color: _textColor6, fontSize: 14)),
-                      style: ElevatedButton.styleFrom(primary: _warna6),
-                    ),
-                    SizedBox(width: 10.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _warna5 = (_warna7 == Colors.white) ? const Color.fromRGBO(244, 81, 141, 1) : Colors.white;
-                          _textColor5 = (_textColor7 == Colors.black) ? Colors.white : Colors.black;
-                        });
-                      }, 
-                      child: Text('Advance level', style: TextStyle(color: _textColor7, fontSize: 14)),
-                      style: ElevatedButton.styleFrom(primary: _warna7),
-                    ),
-                    SizedBox(width: 10.0),
-                    ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _warna5 = (_warna8 == Colors.white) ? const Color.fromRGBO(244, 81, 141, 1) : Colors.white;
-                          _textColor5 = (_textColor8 == Colors.black) ? Colors.white : Colors.black;
-                        });
-                      }, 
-                      child: Text('Entry level', style: TextStyle(color: _textColor8, fontSize: 14)),
-                      style: ElevatedButton.styleFrom(primary: _warna8),
-                    ),
-                  ],
-                ),
-              )
             ],
           ),
         ),
