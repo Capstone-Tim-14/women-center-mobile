8e736a8274e308c58559efff2593acb233319ec8
Putri Diana Hafsyawati <101302312+mputt26@users.noreply.github.com>
2023-11-23 19:27:55 +0700

    add branch metode pembayaran 1 dan 2

---
+++ women_center_mobile/lib/View/metode_pembayaran/metode_pembayaran_1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MetodePembayaran1 extends StatefulWidget {
+  const MetodePembayaran1({super.key});
+
+  @override
+  State<MetodePembayaran1> createState() => _MetodePembayaran1State();
+}
+
+class _MetodePembayaran1State extends State<MetodePembayaran1> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
