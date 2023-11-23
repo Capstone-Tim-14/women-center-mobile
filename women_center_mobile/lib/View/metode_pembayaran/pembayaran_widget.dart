7de9195c86efa29e52b82543f1758e9603cad06e
Putri Diana Hafsyawati <101302312+mputt26@users.noreply.github.com>
2023-11-24 01:47:18 +0700

    payment2 update 1.0

---
+++ women_center_mobile/lib/View/metode_pembayaran/pembayaran_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PeymentWidget2 extends StatelessWidget {
+  const PeymentWidget2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- women_center_mobile/lib/main.dart
+++ women_center_mobile/lib/main.dart
@@ -1,37 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:women_center_mobile/View/artikel/artikel_ku.dart';
 import 'package:women_center_mobile/View/homepage/notifikasi/homepage_notifikasi_3.dart';
+import 'package:women_center_mobile/View/metode_pembayaran/metode_pembayaran_2.dart';
 import 'package:women_center_mobile/View/onboarding/onboarding.dart';
 import 'package:women_center_mobile/View/splash_screen/splash_screen.dart';
 // import 'View/login/login.dart';
 import 'View/register/register.dart';
 
-
-void main(){
+void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title:'women center',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink)
-      ),
-
-    initialRoute: '/artikelku',
-    routes: {
-      '/': (context) => const Splashscreen(),
-      // '/logim': (context) => const Login(),
-      '/register': (context) => const Register(),
-      '/onboarding': (context) => Onboarding(),
-      '/homepagenotif3':(context) => HomepageNotifikasi3(),
-      '/artikelku':(context) => const ArtikelKu()
-    }
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'women center',
+        theme: ThemeData(
+            useMaterial3: true,
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink)),
+        initialRoute: '/payment',
+        routes: {
+          '/': (context) => const Splashscreen(),
+          // '/logim': (context) => const Login(),
+          '/register': (context) => const Register(),
+          '/onboarding': (context) => Onboarding(),
+          '/homepagenotif3': (context) => HomepageNotifikasi3(),
+          '/artikelku': (context) => const ArtikelKu(),
+          '/payment': (context) => const Payment2(),
+        });
   }
 }
