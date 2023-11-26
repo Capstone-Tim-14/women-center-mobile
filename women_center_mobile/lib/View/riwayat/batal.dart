3dd12f0dbb3c2b38256713465a207ff927563021
Xrtion <kicaubae@gmail.com>
2023-11-26 16:27:56 +0700

    Done

--- women_center_mobile/lib/View/riwayat/batal.dart
+++ women_center_mobile/lib/View/riwayat/batal.dart
@@ -148,6 +148,92 @@ class _RiwayatBatalState extends State<RiwayatBatal> {
                 ),
               ),
             ),
+            Card(
+              color: Color(0xFFF8E8EE),
+              margin: EdgeInsets.all(15.0),
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(8.0),
+                              child: Image.asset(
+                                'Assets/images/riwayatbatal2.png',
+                                width: 60.0, // Sesuaikan dengan ukuran gambar yang diinginkan
+                                height: 60.0,
+                                fit: BoxFit.cover, // Sesuaikan dengan kebutuhan tata letak gambar
+                              ),
+                            ),
+                            SizedBox(width:16), 
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Krishna Barbe, M.Psi., Psikolog',
+                                  style: TextStyle(
+                                      fontSize: 16.0, fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  'Psikolog • Universitas Indonesia',
+                                  style: TextStyle(
+                                      fontSize: 16.0, color: Color(0xFFF4518D)),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Divider(),
+                    SizedBox(height: 10),
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.calendar_today, color: Colors.black),
+                            SizedBox(width: 3.0), 
+                            Text(
+                              '02 Oktober 2023', 
+                              style: TextStyle(fontSize: 16.0),
+                            ),
+                            SizedBox(width: 60.0),
+                            Icon(Icons.schedule, color: Colors.black),
+                            SizedBox(width: 8.0), 
+                            Text(
+                              '09:00-11:00', 
+                              style: TextStyle(fontSize: 16.0),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 15),
+                    Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(Icons.cancel, color: Color(0xFFFE10000), size: 27),
+                            SizedBox(width: 3.0), 
+                            Text(
+                              'Dibatalkan', 
+                              style: TextStyle(fontSize: 20.0, color: Color(0xFFFE10000)),
+                            ),
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            
           ],
         ),
       ),
