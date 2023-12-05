b90b8a4ebfff53f59c21d5eaf48dfbfd66f800c8
Neuxtron <71965568+Neuxtron@users.noreply.github.com>
2023-12-05 08:50:31 +0700

    perbaiki eror

--- women_center_mobile/lib/main.dart
+++ women_center_mobile/lib/main.dart
@@ -17,36 +17,33 @@ import 'View/register/register.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key});
+  const MyApp({super.key});
 
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'women center',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
-          textTheme: GoogleFonts.ralewayTextTheme(
-            Theme.of(context).textTheme,
-          ),
+      debugShowCheckedModeBanner: false,
+      title: 'women center',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+        textTheme: GoogleFonts.ralewayTextTheme(
+          Theme.of(context).textTheme,
         ),
-        initialRoute: '/onboarding',
-        routes: {
-          '/login': (context) => const LoginView(),
-          '/register': (context) => const Register(),
-          '/onboarding': (context) => Onboarding(),
-          '/homepagenotif3': (context) => HomepageNotifikasi3(),
-          '/artikelku': (context) => const ArtikelKu(),
-          '/artikel': (context) => const Artikel(),
-        });
-
+      ),
+      initialRoute: '/onboarding',
+      routes: {
+        '/login': (context) => const LoginView(),
+        '/register': (context) => const Register(),
+        '/onboarding': (context) => Onboarding(),
+        '/homepagenotif3': (context) => HomepageNotifikasi3(),
+        '/artikelku': (context) => const ArtikelKu(),
+        '/artikel': (context) => const Artikel(),
+      },
+    );
   }
 }
