c92fd312e1547aa3bd781305bc94a8d73d486ed4
juharmansah <juharmansah0603@gmail.com>
2023-12-07 19:41:21 +0700

    pembaruan

--- women_center_mobile/lib/View/onboarding/onboarding.dart
+++ women_center_mobile/lib/View/onboarding/onboarding.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
   @override
   _OnboardingState createState() => _OnboardingState();
 }
