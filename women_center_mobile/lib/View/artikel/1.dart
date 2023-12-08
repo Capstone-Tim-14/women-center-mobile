608fc9d9a9efdf6106b0d8520ef5afdf4e567d82
rafitrhmn <rafitaufih10@gmail.com>
2023-12-09 00:59:45 +0700

    update 1.0

--- women_center_mobile/lib/View/artikel/1.dart
+++ women_center_mobile/lib/View/artikel/1.dart
@@ -1,36 +1,36 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_quill/flutter_quill.dart';
+// import 'package:flutter/material.dart';
+// // import 'package:flutter_quill/flutter_quill.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: QuillEditorScreen(),
-    );
-  }
-}
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: QuillEditorScreen(),
+//     );
+//   }
+// }
 
-class QuillEditorScreen extends StatefulWidget {
-  @override
-  _QuillEditorScreenState createState() => _QuillEditorScreenState();
-}
+// class QuillEditorScreen extends StatefulWidget {
+//   @override
+//   _QuillEditorScreenState createState() => _QuillEditorScreenState();
+// }
 
-class _QuillEditorScreenState extends State<QuillEditorScreen> {
-  final QuillController _controller = QuillController.basic();
+// class _QuillEditorScreenState extends State<QuillEditorScreen> {
+//   final QuillController _controller = QuillController.basic();
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Quill Editor'),
-      ),
-      body: Center(
-        child: Container(margin: EdgeInsets.all(8.0), child: Container()),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Quill Editor'),
+//       ),
+//       body: Center(
+//         child: Container(margin: EdgeInsets.all(8.0), child: Container()),
+//       ),
+//     );
+//   }
+// }
--- women_center_mobile/lib/View/artikel/edit_artikel.dart
+++ women_center_mobile/lib/View/artikel/edit_artikel.dart
@@ -40,28 +40,53 @@ class _buat_artikelState extends State<buat_artikel> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.pink[100],
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          color: Colors.black,
-          onPressed: () {},
+        elevation: 0,
+        title: const Text(
+          'Edit Artikel',
+          style: const TextStyle(
+            fontFamily: "Roboto",
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            color: Color(0xff0b0b0b),
+            height: 20 / 14,
+          ),
         ),
-        title: const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Edit Artikel",
-              style: TextStyle(
-                color: Color(0xFF0B0B0B),
-                fontSize: 14,
-                // fontFamily: 'Roboto',
-                fontWeight: FontWeight.w500,
-                height: 0.10,
-                letterSpacing: 0.14,
-              ),
-            ),
-          ],
+        centerTitle: true, // Judul app bar
+        leading: IconButton(
+          // Tombol kembali di sebelah kiri
+          icon: const Icon(Icons.arrow_back), color: Colors.black,
+          onPressed: () {
+            // Aksi ketika tombol kembali ditekan
+            // Misalnya, kembali ke layar sebelumnya
+            // Navigator.pop(context);
+          },
         ),
       ),
+      // appBar: AppBar(
+      //   backgroundColor: Colors.pink[100],
+      //   leading: IconButton(
+      //     icon: const Icon(Icons.arrow_back),
+      //     color: Colors.black,
+      //     onPressed: () {},
+      //   ),
+      //   title: const Row(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       Text(
+      //         "Edit Artikel",
+      //         style: TextStyle(
+      //           color: Color(0xFF0B0B0B),
+      //           fontSize: 14,
+      //           // fontFamily: 'Roboto',
+      //           fontWeight: FontWeight.w500,
+      //           // height: 0.10,
+      //           // letterSpacing: 0.14,
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      //   centerTitle: true,
+      // ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.only(left: 20, top: 20),
@@ -94,11 +119,17 @@ class _buat_artikelState extends State<buat_artikel> {
                     right: 10,
                     bottom: 10,
                   ),
-                  decoration: ShapeDecoration(
+                  decoration: BoxDecoration(
                     color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 3) // Perpindahan bayangan (x, y)
+                          ),
+                    ],
                   ),
                   child: TextFormField(
                     style: const TextStyle(
@@ -121,7 +152,6 @@ class _buat_artikelState extends State<buat_artikel> {
                     ),
                   ),
                 ),
-
                 const SizedBox(
                   height: 14,
                 ),
@@ -150,11 +180,18 @@ class _buat_artikelState extends State<buat_artikel> {
                         right: 10,
                         bottom: 10,
                       ),
-                      decoration: ShapeDecoration(
+                      decoration: BoxDecoration(
                         color: Colors.white,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.grey.withOpacity(0.2),
+                              spreadRadius: 2,
+                              blurRadius: 5,
+                              offset:
+                                  Offset(0, 3) // Perpindahan bayangan (x, y)
+                              ),
+                        ],
                       ),
                       child: Row(
                         mainAxisSize: MainAxisSize.min,
@@ -204,7 +241,7 @@ class _buat_artikelState extends State<buat_artikel> {
                     ),
                   ],
                 ),
-                const SizedBox(height: 8),
+                const SizedBox(height: 14),
                 Container(
                   width: 360,
                   height: 540,
@@ -233,10 +270,18 @@ class _buat_artikelState extends State<buat_artikel> {
                           right: 10,
                           bottom: 10,
                         ),
-                        decoration: ShapeDecoration(
+                        decoration: BoxDecoration(
                           color: Colors.white,
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8)),
+                          borderRadius: BorderRadius.circular(8),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey.withOpacity(0.2),
+                                spreadRadius: 2,
+                                blurRadius: 5,
+                                offset:
+                                    Offset(0, 3) // Perpindahan bayangan (x, y)
+                                ),
+                          ],
                         ),
                         child: const Column(
                           mainAxisAlignment: MainAxisAlignment.start,
@@ -288,138 +333,7 @@ class _buat_artikelState extends State<buat_artikel> {
                 const SizedBox(
                   height: 20,
                 ),
-                Row(
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: List.generate(
-                        teksList.length,
-                        (index) {
-                          bool isTapped = false;
-
-                          return GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                isTapped = !isTapped;
-                              });
-                            },
-                            child: Container(
-                              margin: const EdgeInsets.only(bottom: 17),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  InkWell(
-                                    onTap: () {
-                                      setState(() {
-                                        isTapped = !isTapped;
-                                      });
-                                    },
-                                    child: Container(
-                                      width: 117,
-                                      height: 31,
-                                      clipBehavior: Clip.antiAlias,
-                                      decoration: ShapeDecoration(
-                                        color: isTapped
-                                            ? const Color.fromARGB(
-                                                255, 217, 60, 99)
-                                            : const Color(0xFFF9F5F6),
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(100),
-                                        ),
-                                      ),
-                                      child: Center(
-                                        child: Text(
-                                          teksList[index],
-                                          style: TextStyle(
-                                            color: isTapped
-                                                ? Colors.white
-                                                : const Color(0xFF636363),
-                                            fontSize: 14,
-                                            fontFamily: 'Raleway',
-                                            fontWeight: FontWeight.w700,
-                                            height: 0,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: List.generate(
-                        teksList.length,
-                        (index) {
-                          bool isTapped = false;
-
-                          return GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                isTapped = !isTapped;
-                              });
-                            },
-                            child: Container(
-                              margin: const EdgeInsets.only(bottom: 17),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  InkWell(
-                                    onTap: () {
-                                      setState(() {
-                                        isTapped = !isTapped;
-                                      });
-                                    },
-                                    child: Container(
-                                      width: 117,
-                                      height: 31,
-                                      clipBehavior: Clip.antiAlias,
-                                      decoration: ShapeDecoration(
-                                        color: isTapped
-                                            ? Colors.pink
-                                            : const Color(0xFFF9F5F6),
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(100),
-                                        ),
-                                      ),
-                                      child: Center(
-                                        child: Text(
-                                          teksList[index],
-                                          style: TextStyle(
-                                            color: isTapped
-                                                ? Colors.white
-                                                : const Color(0xFF636363),
-                                            fontSize: 14,
-                                            fontFamily: 'Raleway',
-                                            fontWeight: FontWeight.w700,
-                                            height: 0,
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
+                TeksListWidget(),
                 const SizedBox(
                   height: 17,
                 ),
@@ -446,55 +360,132 @@ class _buat_artikelState extends State<buat_artikel> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Text(
-                          'Post',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
-                            fontFamily: 'Poppins',
-                            fontWeight: FontWeight.w700,
-                            height: 0.15,
+                        Padding(
+                          padding: EdgeInsets.only(top: 7.5),
+                          child: Text(
+                            'Post',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontFamily: 'Poppins',
+                              fontWeight: FontWeight.w700,
+                              height: 0.15,
+                            ),
                           ),
                         ),
                       ],
                     ),
                   ),
                 )
-
-                // Container(
-                //   width: 360,
-                //   height: 40,
-                //   padding:
-                //       const EdgeInsets.symmetric(horizontal: 105, vertical: 1),
-                //   clipBehavior: Clip.antiAlias,
-                //   decoration: ShapeDecoration(
-                //     color: Color(0xFFF4518D),
-                //     shape: RoundedRectangleBorder(
-                //       borderRadius: BorderRadius.circular(100),
-                //     ),
-                //   ),
-                //   child: Row(
-                //     mainAxisSize: MainAxisSize.min,
-                //     mainAxisAlignment: MainAxisAlignment.center,
-                //     crossAxisAlignment: CrossAxisAlignment.center,
-                //     children: [
-                //       Text(
-                //         'Post',
-                //         style: TextStyle(
-                //           color: Colors.white,
-                //           fontSize: 12,
-                //           fontFamily: 'Poppins',
-                //           fontWeight: FontWeight.w700,
-                //           height: 0.15,
-                //         ),
-                //       ),
-                //     ],
-                //   ),
-                // )
               ],
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class TeksListWidget extends StatefulWidget {
+  @override
+  _TeksListWidgetState createState() => _TeksListWidgetState();
+}
+
+class _TeksListWidgetState extends State<TeksListWidget> {
+  List<String> teksList = [
+    'Teks 1',
+    'Teks 2',
+    'Teks 3',
+    // ... tambahkan teks lainnya di sini
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          child: Padding(
+            padding: const EdgeInsets.only(
+                right: 10), // Sesuaikan dengan jarak yang diinginkan
+            child: _buildColumn(teksList, Colors.red),
+          ),
+        ),
+        SizedBox(
+          width: 7,
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(
+                right: 10), // Sesuaikan dengan jarak yang diinginkan
+            child: _buildColumn(teksList, Colors.pink),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Column _buildColumn(List<String> teksList, Color color) {
+    return Column(
+      children: List.generate(
+        teksList.length,
+        (index) {
+          bool isTapped = false;
+
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                isTapped = !isTapped;
+              });
+            },
+            child: Container(
+              margin: const EdgeInsets.only(bottom: 17),
+              child: Row(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      setState(() {
+                        isTapped = !isTapped;
+                      });
+                    },
+                    child: Container(
+                      width: 117,
+                      height: 31,
+                      clipBehavior: Clip.antiAlias,
+                      decoration: ShapeDecoration(
+                        color:
+                            isTapped ? color : Color.fromARGB(255, 0, 255, 89),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 7),
+                        child: Column(
+                          children: [
+                            Text(
+                              teksList[index],
+                              style: TextStyle(
+                                color: isTapped
+                                    ? Colors.white
+                                    : const Color(0xFF636363),
+                                fontSize: 14,
+                                fontFamily: 'Raleway',
+                                fontWeight: FontWeight.w700,
+                                height: 0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
