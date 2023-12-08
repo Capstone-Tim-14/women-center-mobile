029eb32b7513f66ede44a1fb16b531dccd1cf2d2
juharmansah <juharmansah0603@gmail.com>
2023-12-08 08:08:09 +0700

    update bersihkan development

--- women_center_mobile/lib/main.dart
+++ women_center_mobile/lib/main.dart
@@ -1,6 +1,5 @@
 // JUHARMAN YANG MINTA
 import 'package:flutter/material.dart';
-import 'package:women_center_mobile/View/artikel/buat_artikel.dart';
 import 'package:provider/provider.dart';
 import 'package:women_center_mobile/View/artikel_2/artikel_2.dart';
 import 'package:women_center_mobile/View/homepage/homepage_view.dart';
@@ -12,8 +11,6 @@ import 'package:women_center_mobile/View/artikel/artikel_ku.dart';
 import 'package:women_center_mobile/View/homepage/notifikasi/homepage_notifikasi_3.dart';
 import 'package:women_center_mobile/View/login/login_view.dart';
 import 'package:women_center_mobile/View/onboarding/onboarding.dart';
-import 'View/bottomnavigationbar/main_page.dart';
-import 'View/homepage/homepage_section3.dart';
 import 'package:women_center_mobile/ViewModel/artikel_view_model/artikel_view_model.dart';
 import 'package:women_center_mobile/View/riwayat/batal.dart';
 import 'package:women_center_mobile/View/sesi_konseling/sesi_konseling.dart';
@@ -21,8 +18,9 @@ import 'View/career/detail_job.dart';
 import 'View/career/career.dart';
 import 'View/login/login_view.dart';
 import 'View/register/register.dart';
-import 'View/welcome_page/welcome_page.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'View/welcome_page/welcome_page.dart';
+
 
 void main() {
   runApp(const MyApp());
