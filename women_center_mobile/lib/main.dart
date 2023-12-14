0fd152e90afeb0b2cddc0c49ca62873f6e68c803
rafitrhmn <rafitaufih10@gmail.com>
2023-12-14 21:22:40 +0700

    update main.dart

--- women_center_mobile/lib/main.dart
+++ women_center_mobile/lib/main.dart
@@ -8,11 +8,7 @@ import 'package:women_center_mobile/View/bottomnavigationbar/main_page_konselor.
 import 'package:women_center_mobile/View/chat_konseling/chat_konseling.dart';
 import 'package:women_center_mobile/View/edit_profile/edit_profile.dart';
 import 'package:provider/provider.dart';
-import 'package:women_center_mobile/View/artikel_2/artikel_2.dart';
-import 'package:women_center_mobile/View/homepage/homepage_section3.dart';
-import 'package:women_center_mobile/View/homepage/homepage_view.dart';
 import 'package:women_center_mobile/View/FAQ/bantuan.dart';
-import 'package:women_center_mobile/View/konseling/konseling_pilihan_konselor_2.dart';
 import 'package:women_center_mobile/View/artikel/artikel_user/artikel_view.dart';
 import 'package:women_center_mobile/View/homepage/notifikasi/homepage_notifikasi.dart';
 import 'package:women_center_mobile/View/artikel/artikel_user/artikel_ku.dart';
@@ -34,12 +30,8 @@ import 'package:women_center_mobile/View/sesi_konseling/sesi_konseling.dart';
 import 'package:women_center_mobile/ViewModel/career_view_model/career_view_model.dart';
 import 'package:women_center_mobile/ViewModel/register_view_model/register_view_model.dart';
 import 'View/bottomnavigationbar/main_page.dart';
-import 'View/career/detail_job.dart';
-import 'View/career/career.dart';
 import 'View/konseling/konseling_topik_konseling.dart';
-import 'View/login/login_view.dart';
 import 'View/register/register.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'View/welcome_page/welcome_page.dart';
 
 void main() {
@@ -66,7 +58,7 @@ class MyApp extends StatelessWidget {
             fontFamily: 'Raleway',
             useMaterial3: true,
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink)),
-        initialRoute: '/konseling_view',
+        initialRoute: '/',
         routes: {
           // '/': (context) => Home3(),
           '/': (context) => const Welcome(),
@@ -95,7 +87,7 @@ class MyApp extends StatelessWidget {
           '/sesi_konseling': (context) => SesiKonseling(),
           '/hapus_artikel': (context) => ArticleListPage(),
           '/bantuan': (context) => FAQ(),
-          '/konseling_view': (context) => KonselingSection(),
+          '/konseling_view': (context) => const KonselingSection(),
         },
       ),
     );
