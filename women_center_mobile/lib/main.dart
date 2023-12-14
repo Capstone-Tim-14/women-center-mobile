fdac7b47ebe4e3c1a17b5fd7b3797fe7f01efa3c
xramadhann <xxdhanz@gmail.com>
2023-12-14 17:20:12 +0700

    Done Konselor 2

--- women_center_mobile/lib/main.dart
+++ women_center_mobile/lib/main.dart
@@ -17,6 +17,7 @@ import 'package:women_center_mobile/View/artikel/artikel_user/artikel_view.dart'
 import 'package:women_center_mobile/View/homepage/notifikasi/homepage_notifikasi.dart';
 import 'package:women_center_mobile/View/artikel/artikel_user/artikel_ku.dart';
 import 'package:women_center_mobile/View/homepage/notifikasi/homepage_notifikasi_3.dart';
+import 'package:women_center_mobile/View/konseling/konseling_view.dart';
 import 'package:women_center_mobile/View/metode_pembayaran/metode_pembayaran_1.dart';
 import 'package:women_center_mobile/View/metode_pembayaran/metode_pembayaran_2.dart';
 import 'package:women_center_mobile/View/metode_pembayaran/pembayaran_widget_tabBar.dart';
@@ -63,7 +64,7 @@ class MyApp extends StatelessWidget {
             fontFamily: 'Raleway',
             useMaterial3: true,
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink)),
-        initialRoute: '/',
+        initialRoute: '/konseling_view',
         routes: {
           // '/': (context) => Home3(),
           '/': (context) => const Welcome(),
@@ -92,6 +93,7 @@ class MyApp extends StatelessWidget {
           '/sesi_konseling': (context) => SesiKonseling(),
           '/hapus_artikel': (context) => ArticleListPage(),
           '/bantuan': (context) => FAQ(),
+          '/konseling_view': (context) => KonselingSection(),
         },
       ),
     );
